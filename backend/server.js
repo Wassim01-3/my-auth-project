c939a6d3f5c416ef45e53f80f0412dcaf904ac03
Wassim01-3 <wassimmars0@gmail.com>
2025-04-03 19:43:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,51 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-const authMiddleware = require('./middleware/authMiddleware'); // Import the middleware
 const productRoutes = require('./routes/productRoutes');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
-// Log environment variables
-console.log('MONGO_URI:', process.env.MONGO_URI);
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
-
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-// Middleware
 app.use(express.json());
-
 app.use(cors({
-  origin: 'https://my-auth-project.onrender.com', // Replace with your frontend URL
-  credentials: true, // Allow credentials (cookies)
+  origin: 'https://my-auth-project.onrender.com',
+  credentials: true,
 }));
-// Add with other route middleware
-app.use('/api/products', productRoutes);
 
-
-// Serve static files (frontend)
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Add before your route handlers
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-// Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
-// Serve the index.html file as the default route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Protect the /home route
-app.get('/home', authMiddleware, (req, res) => {
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/home.html'));
 });
 
-// Start the server
-const PORT = process.env.PORT || 10000; // Use the correct port
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
