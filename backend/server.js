de53f95a5f7fb3948fd73a9b5d683c8a354781c9
Wassim01-3 <wassimmars0@gmail.com>
2025-04-03 19:58:02 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,25 +12,19 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-app.use(express.json());
 app.use(cors({
   origin: 'https://my-auth-project.onrender.com',
-  credentials: true,
+  credentials: true
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/home.html'));
-});
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
