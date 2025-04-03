85168704464ad4c5088128fb2e201a1f98b515c3
Wassim01-3 <wassimmars0@gmail.com>
2025-04-03 20:58:40 +0200

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  category: { type: String, required: true },
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  phoneNumber: { type: String, required: true },
-  address: { type: String, required: true },
-  description: { type: String, required: true },
-  images: [{ type: String }],
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    category: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    phoneNumber: { type: String, required: true },
+    address: { type: String, required: true },
+    description: { type: String, required: true },
+    images: [{ type: String }], // Array of image URLs
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Product', productSchema);
